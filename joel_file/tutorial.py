8e127afecf620ef8ea3ddc39b4a9d545a6deff67
vilayatibacchey <vilayatibacchey@gmail.com>
2026-07-29 19:07:06 +0530

    Joel updated this

--- joel_file/tutorial.py
+++ joel_file/tutorial.py
@@ -5,7 +5,7 @@
 
 # These are called comments and almost every coding thingy has them.
 # So, I have made a html file with all that boring code (it's called index.html), and I just want you to experiment with it and run it in your browser or code editor.
-# Also learn the HTML format stuff... I promise this isn't easy.
+# Also learn the HTML format stuff... I promise this isn't easy. Also if the html file doesnt run here, turn off restrict and open in main app.
 
 # Hey, don't forget to save your changes!! Please, click the weird symbol on the sidebar and click commit and push. Also please leave a message in the title.
 # Happy coding. Tip: Try adding a button by typing this <button> Heyo </button>. If you do it, I will add some CSS for the style!
