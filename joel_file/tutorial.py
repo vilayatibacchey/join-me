17091051edddf1f3791de9485562ea2a2c99e7e5
vilayatibacchey <vilayatibacchey@gmail.com>
2026-07-29 18:59:10 +0530

    Joel is done speaking

---
+++ joel_file/tutorial.py
@@ -0,0 +1,11 @@
+# Welcome, if you've made it this far, well done!
+
+# Python isn't what you're going to learn for designing, infact it is so hard to learn.
+# Anything outside these hashtags are considered errors... for some reason.
+
+# These are called comments and almost every coding thingy has them.
+# So, I have made a html file with all that boring code (it's called index.html), and I just want you to experiment with it and run it in your browser or code editor.
+# Also learn the HTML format stuff... I promise this isn't easy.
+
+# Hey, don't forget to save your changes!! Please, click the weird symbol on the sidebar and click commit and push. Also please leave a message in the title.
+# Happy coding. Tip: Try adding a button by typing this <button> Heyo </button>. If you do it, I will add some CSS for the style!
