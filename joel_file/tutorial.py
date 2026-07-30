858eff78de950664ca8ba3aeb9333ef6c9017891
vilayatibacchey <vilayatibacchey@gmail.com>
2026-07-30 16:34:58 +0530

    Joel updated this tutorial

--- joel_file/tutorial.py
+++ joel_file/tutorial.py
@@ -1,11 +1,13 @@
 # Welcome, if you've made it this far, well done!
 
+# Type message here: Hi, I'm ...
+
 # Python isn't what you're going to learn for designing, infact it is so hard to learn.
 # Anything outside these hashtags are considered errors... for some reason.
 
 # These are called comments and almost every coding thingy has them.
 # So, I have made a html file with all that boring code (it's called index.html), and I just want you to experiment with it and run it in your browser or code editor.
-# Also learn the HTML format stuff... I promise this isn't easy. Also if the html file doesnt run here, turn off restrict and open in main app.
+# Also learn the HTML format stuff... I promise this isn't easy. Also if the html file doesnt run here, run it in your main code editor
 
-# Hey, don't forget to save your changes!! Please, click the weird symbol on the sidebar and click commit and push. Also please leave a message in the title.
+# Hey, don't forget to save your changes!! Please, click the weird symbol on the sidebar and click commit and push (if you're in the editor). Also please leave a message in the title.
 # Happy coding. Tip: Try adding a button by typing this <button> Heyo </button>. If you do it, I will add some CSS for the style!
